9e11e21c9e0cf695f6f010d01b4916ac33ba1a01
Homin Lee <homin.lee@suapapa.net>
2016-03-21 14:50:50 +0900

    convunzip: remove iconv dependency

--- convunzip/convunzip.go
+++ convunzip/convunzip.go
@@ -8,56 +8,15 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 
-	iconv "github.com/djimenez/iconv-go"
+	"github.com/suapapa/go_hangul/encoding/cp949"
 )
 
-var encFrom string
-var encTo string
 var dryRun bool
-var converter *iconv.Converter
 
 func init() {
-	flag.StringVar(&encFrom, "f", "auto", "encording of input zip file")
-	flag.StringVar(&encTo, "t", "auto", "encording of the environment")
 	flag.BoolVar(&dryRun, "n", false, "dry-run")
 	flag.Parse()
-
-	var sysLocale, sysEncording string
-
-	envLANG := os.Getenv("LANG")
-	if envLANG == "" {
-		sysLocale = "unknown"
-		sysEncording = "unknown"
-	} else {
-		s := strings.SplitN(envLANG, ".", 2) // example ko_KR.utf8
-		sysLocale = s[0]
-		sysEncording = s[1]
-	}
-
-	if encFrom == "auto" {
-		// guess native encording by system locale
-		// TODO: add more native encordings for locales
-		switch sysLocale {
-		case "ko_KR":
-			encFrom = "cp949"
-		default:
-			encFrom = "ascii"
-		}
-	}
-
-	if encTo == "auto" {
-		switch sysEncording {
-		case "unknown":
-			encTo = "utf8"
-		default:
-			encTo = sysEncording
-		}
-	}
-
-	fmt.Println("conv from", encFrom, "to", encTo)
-	converter, _ = iconv.NewConverter(encFrom, encTo)
 }
 
 func checkIf(err error) {
@@ -72,13 +31,15 @@ func convUnzip(fileName string) {
 	defer r.Close()
 
 	for _, f := range r.File {
-		fixed, _ := converter.ConvertString(f.Name)
+		fixedBytes, err := cp949.From([]byte(f.Name))
+		checkIf(err)
+		fixed := string(fixedBytes)
 		fmt.Println(fixed)
 		if dryRun {
 			continue
 		}
 
-		err := os.MkdirAll(filepath.Dir(fixed), os.ModePerm)
+		err = os.MkdirAll(filepath.Dir(fixed), os.ModePerm)
 		checkIf(err)
 
 		if f.FileInfo().IsDir() {
