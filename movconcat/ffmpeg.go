9942fb3a9c0b2008987455c671f63d710569d2dd
Homin Lee <homin.lee@suapapa.net>
2016-11-09 00:56:44 +0900

    movconcat: add flag for choose serial/parallel run

---
+++ movconcat/ffmpeg.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func runFFmpeg(k string, v []string) {
+	tmp, err := os.Create(k + ".list")
+	panicIfErr(err)
+	defer os.Remove(tmp.Name())
+
+	log.Println("Create file list,", tmp.Name())
+	for _, f := range v {
+		fmt.Fprintln(tmp, "file", f)
+	}
+	tmp.Close()
+
+	o := k + ".mov"
+
+	stdErr, err := os.Create(k + ".log")
+	panicIfErr(err)
+	defer stdErr.Close()
+
+	var cmd *exec.Cmd
+	if *flagUseDocker {
+		// cf) https://github.com/jrottenberg/ffmpeg
+		// $ sudo docker run -v $PWD:/opt/data --rm \
+		// jrottenberg/ffmpeg -f concat \
+		// -i /opt/data/files.list -c copy /opt/data/test.mov
+		cmd = exec.Command("docker", "run", "--rm",
+			"-v", os.Getenv("PWD")+":/opt/data",
+			"jrottenberg/ffmpeg",
+			"-f", "concat",
+			"-i", filepath.Join("/opt/data", tmp.Name()),
+			"-c", "copy",
+			filepath.Join("/opt/data", o),
+		)
+	} else {
+		cmd = exec.Command("ffmpeg",
+			"-f", "concat",
+			"-i", tmp.Name(),
+			"-c", "copy",
+			o,
+		)
+	}
+
+	cmd.Stderr = stdErr
+
+	err = cmd.Start()
+	panicIfErr(err)
+
+	log.Println("concatting", o, "...")
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Printf("failed to concat %s with error: %v\n", o, err)
+	} else {
+		log.Println(o, "finished.")
+	}
+}
--- movconcat/main.go
+++ movconcat/main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
-	"os"
-	"os/exec"
 	"path/filepath"
 	"regexp"
 	"sort"
@@ -17,7 +14,8 @@ const movRePtn = `(\d\d\d\d_\d\d\d\d_\d\d\d\d\d\d)_\d\d\d.MOV`
 const movTimeForm = "2006_0102_150405"
 
 var (
-	flagUseDocker = flag.Bool("d", false, "use docker")
+	flagUseDocker  = flag.Bool("d", false, "use docker")
+	flagParalleRun = flag.Bool("p", false, "run cmd parallely")
 )
 
 func main() {
@@ -63,63 +61,15 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(len(movs))
 	for k, v := range movs {
-		go func(k string, v []string) {
-			defer wg.Done()
-
-			tmp, err := os.Create(k + ".list")
-			panicIfErr(err)
-			defer os.Remove(tmp.Name())
-
-			log.Println("Create file list,", tmp.Name())
-			for _, f := range v {
-				fmt.Fprintln(tmp, "file", f)
-			}
-			tmp.Close()
-
-			o := k + ".mov"
-
-			stdErr, err := os.Create(k + ".log")
-			panicIfErr(err)
-			defer stdErr.Close()
-
-			var cmd *exec.Cmd
-			if *flagUseDocker {
-				// cf) https://github.com/jrottenberg/ffmpeg
-				// $ sudo docker run -v $PWD:/opt/data --rm \
-				// jrottenberg/ffmpeg -f concat \
-				// -i /opt/data/files.list -c copy /opt/data/test.mov
-				cmd = exec.Command("docker", "run", "--rm",
-					"-v", os.Getenv("PWD")+":/opt/data",
-					"jrottenberg/ffmpeg",
-					"-f", "concat",
-					"-i", filepath.Join("/opt/data", tmp.Name()),
-					"-c", "copy",
-					filepath.Join("/opt/data", o),
-				)
-			} else {
-				cmd = exec.Command("ffmpeg",
-					"-f", "concat",
-					"-i", tmp.Name(),
-					"-c", "copy",
-					o,
-				)
-			}
-
-			cmd.Stderr = stdErr
-
-			err = cmd.Start()
-			panicIfErr(err)
-
-			log.Println("concatting", o, "...")
-
-			err = cmd.Wait()
-			if err != nil {
-				log.Printf("failed to concat %s with error: %v\n", o, err)
-			} else {
-				log.Println(o, "finished.")
-			}
-		}(k, v)
-
+		if *flagParalleRun {
+			go func(k string, v []string) {
+				runFFmpeg(k, v)
+				wg.Done()
+			}(k, v)
+		} else {
+			runFFmpeg(k, v)
+			wg.Done()
+		}
 	}
 
 	wg.Wait()
