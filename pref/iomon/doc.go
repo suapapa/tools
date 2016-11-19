8b7424a8d27822ab188bf820c164ebdb59f5662b
Homin Lee <homin.lee@suapapa.net>
2016-11-19 22:45:25 +0900

    iomon: remove

--- pref/iomon/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2015, Homin Lee. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package iomon provide io.Reader and io.Writer which has...
-package iomon
