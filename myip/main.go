32894fa29d241c3567df3280f8b6fc4d19a68973
Homin Lee <homin.lee@suapapa.net>
2016-12-06 16:31:48 +0900

    myip: add feature to masuring transper speed

--- myip/main.go
+++ myip/main.go
@@ -1,22 +1,29 @@
 package main
 
 import (
+	"crypto/rand"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
+	"time"
+
+	tachoio "github.com/suapapa/go_tachoio"
 )
 
 var (
-	flagPort   string
-	flagServer bool
+	flagPort              string
+	flagServer            bool
+	flagSpeedTestDuration int
 )
 
 func init() {
 	flag.StringVar(&flagPort, "p", "8081", "server port")
-	flag.BoolVar(&flagServer, "s", false, "print client IP")
+	flag.BoolVar(&flagServer, "s", false, "print IP from client")
+	flag.IntVar(&flagSpeedTestDuration, "d", 0, "speed test duration in secs")
 	flag.Parse()
 }
 
@@ -38,7 +45,22 @@ func main() {
 			log.Printf("Received message %s -> %s\n",
 				conn.RemoteAddr(), conn.LocalAddr())
 
-			go handleRequest(conn)
+			go func(conn net.Conn) {
+				defer conn.Close()
+
+				// if flagSpeedTestDuration == 0 {
+				// 	_, err := io.Copy(os.Stdout, conn)
+				// 	if err != nil {
+				// 		log.Println("error at read:", err)
+				// 	}
+				// } else {
+				log.Println("waiting for all received...")
+				tr := tachoio.NewReader(conn)
+				io.Copy(ioutil.Discard, tr)
+				n, d := tr.ReadMeter()
+				log.Printf("read %.1f bytes per a sec\n", float64(n)/d.Seconds())
+				// }
+			}(conn)
 		}
 	} else { // client
 		ip, mac, err := resolveIP()
@@ -60,20 +82,21 @@ func main() {
 		} else {
 			c = os.Stdout
 		}
+		defer c.Close()
 
-		fmt.Fprintf(c, "IP: %s\nMAC: %s\n", ip, mac)
-		c.Close()
+		if flagSpeedTestDuration == 0 {
+			fmt.Fprintf(c, "IP: %s\nMAC: %s\n", ip, mac)
+		} else {
+			secTick := time.Tick(time.Second)
+			tw := tachoio.NewWriter(c)
+			go func() { io.Copy(tw, rand.Reader) }()
+			for i := 0; i < flagSpeedTestDuration; i++ {
+				select {
+				case <-secTick:
+					n, d := tw.WriteMeter()
+					log.Printf("write %.1f bytes per a sec\n", float64(n)/d.Seconds())
+				}
+			}
+		}
 	}
-}
-
-func handleRequest(conn net.Conn) {
-	defer conn.Close()
-	buf := make([]byte, 1024)
-	reqLen, err := conn.Read(buf)
-	if err != nil {
-		log.Println("error at read:", err)
-		return
-	}
-
-	fmt.Println(string(buf[:reqLen]))
 }
