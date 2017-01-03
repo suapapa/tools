1236049265f333f8e1d3c33464816078df54d56b
Homin Lee <homin.lee@suapapa.net>
2017-01-04 00:25:37 +0900

    movconcat: add filename grouping function for gopro fn convention

---
+++ movconcat/gp_naming.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+const gpFileNumRePtn = `GOPR(\d\d\d\d).MP4`
+const gpChapNumRePtn = `GP(\d\d)(\d\d\d\d).MP4`
+
+var (
+	gpFileNumRe = regexp.MustCompile(gpFileNumRePtn)
+	gpChapNumRe = regexp.MustCompile(gpChapNumRePtn)
+)
+
+// gpFileChaper makes filename map of file number
+func gpFileChaper(files []string) map[string][]string {
+	r := make(map[string][]string)
+	for _, f := range files {
+		// single video
+		if strings.HasPrefix(f, "GOPR") {
+			m := gpFileNumRe.FindStringSubmatch(f)
+			if len(m) == 2 {
+				fn := m[1]
+				r[fn] = append(r[fn], f)
+			}
+			continue
+		}
+
+		// chapterd videos
+		if strings.HasPrefix(f, "GP") {
+			m := gpChapNumRe.FindStringSubmatch(f)
+			if len(m) == 3 {
+				fn := m[2]
+				r[fn] = append(r[fn], f)
+
+			}
+
+		}
+	}
+
+	return r
+}
---
+++ movconcat/gp_naming_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestGpFileChaper(t *testing.T) {
+	fs := strings.Split("GOPR0021.MP4 GP010021.MP4 GP020021.MP4 GP030021.MP4 GP040021.MP4", " ")
+
+	got := gpFileChaper(fs)
+
+	expect := map[string][]string{
+		"0021": []string{"GOPR0021.MP4", "GP010021.MP4", "GP020021.MP4", "GP030021.MP4", "GP040021.MP4"},
+	}
+
+	if !reflect.DeepEqual(got, expect) {
+		t.Errorf("expect %v, got %v\n", expect, got)
+	}
+}
