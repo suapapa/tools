622a3bb1336cc2ec29ce139385ac70ee062d5f0e
Homin Lee <homin.lee@suapapa.net>
2017-03-29 20:40:47 +0900

    com: fix to handle syscalls

--- com/main.go
+++ com/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/tarm/serial"
 )
@@ -16,24 +19,23 @@ func main() {
 	pn := os.Args[1]
 	s, err := serial.OpenPort(&serial.Config{
 		Name: pn,
-		Baud: 115200,
+		Baud: 9600,
 	})
 	if err != nil {
 		panic(err)
 	}
 
-	rBuff := make([]byte, 1)
-	wBuff := make([]byte, 1)
-
 	quitC := make(chan struct{})
 	ctx, cancle := context.WithCancel(context.Background())
-	defer cancle()
 
 	go func(ctx context.Context) {
+		rBuff := make([]byte, 1)
+
 	loopR:
 		for {
 			select {
 			case <-ctx.Done():
+				log.Println("quit loopR")
 				break loopR
 			default:
 				s.Read(rBuff)
@@ -44,10 +46,13 @@ func main() {
 	}(ctx)
 
 	go func(ctx context.Context) {
+		wBuff := make([]byte, 1)
+
 	loopW:
 		for {
 			select {
 			case <-ctx.Done():
+				log.Println("quit loopW")
 				break loopW
 			default:
 				os.Stdin.Read(wBuff)
@@ -56,6 +61,14 @@ func main() {
 		}
 		quitC <- struct{}{}
 	}(ctx)
+
+	sigC := make(chan os.Signal, 1)
+	signal.Notify(sigC, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigC
+		cancle()
+	}()
 
 	<-quitC
 	<-quitC
