ab1fb76b968f345c97ce9c607f27d2bd9eadc50c
Homin Lee <homin.lee@suapapa.net>
2017-08-02 14:07:11 +0900

    weather: get with []byte

--- weather/get.go
+++ weather/get.go
@@ -17,35 +17,35 @@ var (
 	appKey = os.Getenv("SK_WEATHER_APIKEY")
 )
 
-func getGPS(lat, lon int) (string, error) {
+func getGPS(lat, lon int) ([]byte, error) {
 	url := fmt.Sprintf(urlFmtGPS, lat, lon)
 	return getURL(url)
 }
 
-func getAddr(city, country, village string) (string, error) {
+func getAddr(city, country, village string) ([]byte, error) {
 	url := fmt.Sprintf(urlFmtAddr, city, country, village)
 	return getURL(url)
 }
 
-func getID(id int) (string, error) {
+func getID(id int) ([]byte, error) {
 	url := fmt.Sprintf(urlFmtID, id)
 	return getURL(url)
 }
 
-func getURL(url string) (string, error) {
+func getURL(url string) ([]byte, error) {
 	req, err := http.NewRequest("GET", url, nil)
 
 	req.Header.Set("appKey", appKey)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	s, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return string(s), nil
+	return s, nil
 }
--- weather/main.go
+++ weather/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	log.Println(getID(116))
+	j, _ := getID(116)
+	// fmt.Println(j)
+
+	var w Success
+	err := json.Unmarshal(j, &w)
+	if err != nil {
+		var f Fail
+		json.Unmarshal(j, &f)
+		log.Println(f)
+		os.Exit(2)
+	}
+
+	fmt.Printf("%#v\n", w)
 }
