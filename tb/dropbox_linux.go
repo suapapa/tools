41526408fb4747628c2ea0be9c1b517e9a28504f
Homin Lee <homin.lee@suapapa.net>
2017-08-10 13:39:47 +0900

    tb: add files to find dropbox directory

---
+++ tb/dropbox_linux.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func dropboxDir() string {
+	tomboyRoot := filepath.Join(os.Getenv("HOME"), "Dropbox")
+	return tomboyRoot
+}
---
+++ tb/dropbox_windows.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"os/user"
+	"path/filepath"
+)
+
+func dropboxDir() string {
+	usr, _ := user.Current()
+	return filepath.Join(usr.HomeDir, "Dropbox")
+}
